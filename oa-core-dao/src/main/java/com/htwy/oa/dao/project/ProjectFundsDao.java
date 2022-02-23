9d801e390486ed5ad1d71143eb5927fc4237a655
weiwei.xu <1056068757@qq.com>
2022-02-23 16:11:29 +0800

    项目款

---
+++ oa-core-dao/src/main/java/com/htwy/oa/dao/project/ProjectFundsDao.java
@@ -0,0 +1,9 @@
+package com.htwy.oa.dao.project;
+
+/**
+ * description
+ *
+ * @author Administrator 2022/02/21 16:34
+ */
+public interface ProjectFundsDao {
+}
---
+++ oa-core/src/main/java/com/htwy/oa/controller/project/ProjectFundsController.java
@@ -0,0 +1,9 @@
+/**
+ * @author Vv
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/2/21 0021 16:37
+ */
+public class ProjectFundsController {
+
+}
